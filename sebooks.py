453ff8a8cb0e5ee0c4bb38fc9b6f6fe375d71c34
slaily <slavov.iliyan96@gmail.com>
2020-04-03 22:08:41 +0300

    Add Prometheus WSGI application through the application dispatching

--- sebooks.py
+++ sebooks.py
@@ -1,7 +1,13 @@
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
 from app import create_app
 from app.routes import main
 
 
 app = create_app()
 app.register_blueprint(main)
-app.run()
+# Add prometheus wsgi middleware to route /metrics requests
+app_dispatcher = DispatcherMiddleware(app, {
+    '/metrics': make_wsgi_app()
+})
