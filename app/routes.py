97fa954871ba2f5262d82c210cb20f70d4d2d507
slaily <slavov.iliyan96@gmail.com>
2020-04-04 18:05:44 +0300

    Add measuring request time on search route

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ import requests
 
 from flask import Blueprint
 
+from app import metrics
 from app.utils import extract_query
 from app.search import search_books_by_title
 
@@ -10,6 +11,7 @@ main = Blueprint('main', __name__)
 
 
 @main.route('/search')
+@metrics.request_summary.time()
 def search():
     query = search_books_by_title('The')
     books_kwargs = extract_query(query)
