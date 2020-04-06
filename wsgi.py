f0beb39077a5cad03f05dd27ce6c13f99b20a355
slaily <slavov.iliyan96@gmail.com>
2020-04-06 14:23:21 +0300

    Rename and refactor application file

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,9 @@ from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from app import create_app
-from app.routes import main
 
 
 app = create_app()
-app.register_blueprint(main)
 # Add prometheus wsgi middleware to route /metrics requests
 app_dispatcher = DispatcherMiddleware(app, {
     '/metrics': make_wsgi_app()
